c7204e5eb6b1b145260432e0949317ea085a6236
Michael D Adams <michael.d.adams@gmail.com>
2014-02-20 16:53:22 -0800

    add to tests

--- test/test.binary.js
+++ test/test.binary.js
@@ -6,30 +6,9 @@ test('req.responseType( "arraybuffer" )', function(next){
   request('GET', 'test/transparent.png').responseType('arraybuffer').end(function(res){
     assert(res instanceof request.Response, 'respond with Response');
     assert(res.ok, 'response should be ok');
-    assert(res.text instanceof  , 'res.text');
-    next();
-  });
-
-  var port = window.location.port; || 80;
-  var hostname = window.location.hostname;
-
-  request
-  .get('http://' + hostname + ':' + port + '/xdomain')
-  .withCredentials()
-  .end(function(res){
-    assert(200 == res.status);
-    assert('tobi' == res.text);
-    next();
-  })
-})
-
-test('x-domain failure', function(next){
-  request
-  .get('http://google.com')
-  .end(function(err, res){
-    assert(err, 'error missing');
-    assert(err.crossDomain, 'not .crossDomain');
+    assert(res.text === null, 'res.text is null');
+    assert(res.body instanceof ArrayBuffer, 'res.body is ArrayBuffer');
+    assert(true === false, 'should fail');
     next();
   });
 });
-
